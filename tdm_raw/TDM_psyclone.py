6088569d12ee61cb6fc4fec9422bd9c18e603fdf
orndavid <orn.david@gmail.com>
2018-02-14 10:03:36 +0000

    Fixing minor issues with xml and tdm_raw.py

--- tdm_raw/TDM_psyclone.py
+++ tdm_raw/TDM_psyclone.py
@@ -4,7 +4,7 @@
 #     Created By          :     david
 #     Email               :     david@iiim.is
 #     Creation Date       :     [2018-02-12 17:13]
-#     Last Modified       :     [2018-02-13 15:31]
+#     Last Modified       :     [2018-02-14 10:01]
 #     Description         :     A shorter verion of the TDM used for unofficial
 #                               demo 2
 #     Version             :     1.0
@@ -28,23 +28,23 @@ def PsyCrank(apilink):
                 # Read values from Nunace and store
                 # in tdm until it's tdm's time to
                 # answer
-                words = msg.getString("UtterancUtterancee")
+                words = msg.getString("Utterance")
                 tdm_obj.input_words(words)
             elif trigger_name == "MyTurn":
                 res_msg = tdm_obj.run()
                 if res_msg["Result"] == "OutMSg":
-                    api.postOutputMessage("OutputMsg", res_msg["Text"])
+                    api.postOutputMessage("Utterance", res_msg["Text"])
                 elif res_msg["Result"] == "move":
                     # Send message for robot to move
                     # currently no response or timeout
                     if res_msg["Point"] == "A":
                         api.postOutputMessage("perform_task", res_msg["msg"])
-                        api.postOutputMessage("OutputMsg", res_msg["Text"])
+                        api.postOutputMessage("Utterance", res_msg["Text"])
                     elif res_msg["Point"] == "B":
                         api.postOutputMessage("perform_task", res_msg["msg"])
-                        api.postOutputMessage("OutputMsg", res_msg["Text"])
+                        api.postOutputMessage("Utterance", res_msg["Text"])
                     elif res_msg["Point"] == "C":
                         api.postOutputMessage("perform_task", res_msg["msg"])
-                        api.postOutputMessage("OutputMsg", res_msg["Text"])
+                        api.postOutputMessage("Utterance", res_msg["Text"])
 
 
--- tdm_raw/tdm_raw.py
+++ tdm_raw/tdm_raw.py
@@ -4,7 +4,7 @@
 #     Created By          :     david
 #     Email               :     david@iiim.is
 #     Creation Date       :     [2018-02-12 17:18]
-#     Last Modified       :     [2018-02-13 15:18]
+#     Last Modified       :     [2018-02-14 09:26]
 #     Description         :     Interface object. Stores information 
 #                                and returns message outputs in psyclone form
 #     Version             :     1.0
@@ -87,7 +87,7 @@ def no_value_found_msg():
     Create the right psyclone response
     """
     msg = cmsdk2.DataMessage()
-    msg.setString("Unable to comply")
+    msg.setString("OutputMsg","Unable to comply")
     return {"Result":"OutMsg", "Text":msg}
 
 
@@ -98,8 +98,9 @@ def hello_msg():
                "Destroy, destory, DESTOY"]
     no = np.random.randint(0,len(possible))
     msg = cmsdk2.DataMessage()
-    msg.setString(possible[no])
+    msg.setString("OutputMsg", possible[no])
     return {"Result":"OutMsg", "Text":msg}
+
 
 def go_to_pointA():
     msg = cmsdk2.DataMessage()
@@ -109,8 +110,9 @@ def go_to_pointA():
     msg.setInt("Timeout", 30000)
 
     msg_2 = cmsdk2.DataMessage()
-    msg_2.setString("Sending other to point A")
+    msg_2.setString("OutputMsg","Sending other to point A")
     return {"Result":"Move", "Point":"A", "msg":msg, "Text":msg_2}
+
 
 def go_to_pointB():
     msg = cmsdk2.DataMessage()
@@ -120,8 +122,9 @@ def go_to_pointB():
     msg.setInt("Timeout", 30000)
 
     msg_2 = cmsdk2.DataMessage()
-    msg_2.setString("Sending other to point B")
+    msg_2.setString("OutputMsg","Sending other to point B")
     return {"Result":"Move", "Point":"B", "msg":msg, "Text":msg_2}
+
 
 def go_to_pointC():
     msg = cmsdk2.DataMessage()
@@ -131,8 +134,9 @@ def go_to_pointC():
     msg.setInt("Timeout", 30000)
 
     msg_2 = cmsdk2.DataMessage()
-    msg_2.setString("Sending other to point C")
+    msg_2.setString("OutputMsg","Sending other to point C")
     return {"Result":"Move", "Point":"C", "msg":msg, "Text":msg_2}
+
 
 
 if __name__ == "__main__":
@@ -148,12 +152,10 @@ if __name__ == "__main__":
         obj.input_words(sentence)
         obj.logger.debug("Word_buffer: {}".format(obj.word_buffer))
         out_msg = obj.run()
-        print "main out msg: {}".format(out_msg)
-        print "Output type : {}".format(out_msg["Result"])
         if out_msg["Result"] == "OutMsg":
-            print "Output msg : {}".format(out_msg["Text"])
+            print "Output msg : {}".format(out_msg["Text"].getString("OutputMsg"))
         elif out_msg["Result"] == "Move":
-            print "Output msg : {}".format(out_msg["Point"])
+            print "Output msg : {}".format(out_msg["msg"].getString("PointName"))
 
     print 20*'*'
     print "\t\t Debug finished"
